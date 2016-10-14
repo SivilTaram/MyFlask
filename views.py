4b56637d719a3001858b5e3b00ac80d924e8ea99
SivilTaram <qianlxc@outlook.com>
2016-10-14 13:37:15 +0800

    Add the geturl method for Weixin auth.

--- views.py
+++ views.py
@@ -35,3 +35,23 @@ def about():
         year=datetime.now().year,
         message='Your application description page.'
     )
+
+@app.route('/geturl',methods=['GET'])
+def getUrl():
+    sCorpID = "wxf29b16f0c55ae5f2"
+    sToken = "wzgyXSnxoKNDRb5yoJVtLk4XNkXbKRFm"
+    sEncodingAESKey = "7fJlKWPElDjOtXbYkpkLAeac6eVAuUrrYM2dBWeHvlt"
+    wxcpt = WXBizMsgCrypt(sToken,sEncodingAESKey,sCorpID)
+    params = request.args
+    sVerMsgSig = params.get('msg_signature')
+    sVerMsgTimeStap = params.get('timestamp')
+    sVerNonce= params.get('nonce')
+    sVerEchoStr = params.get('echostr')
+    ret,sEchoStr = wxcpt.VerifyURL(sVerMsgSig,sVerMsgTimeStap,sVerNonce,sVerEchoStr)
+    if ret!=0:
+        print ret
+        return None
+    else:
+        return sEchoStr
+
+
