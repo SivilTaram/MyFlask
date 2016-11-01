0325d1b62138dcaf4e44112675ad668891c84b07
SivilTaram <qianlxc@outlook.com>
2016-11-01 20:44:27 +0800

    Change path

--- views/api.py
+++ views/api.py
@@ -6,12 +6,12 @@ api = Blueprint('api',__name__)
 class TestClass(object):
     def reply_func(self,param_dict):
         print ('Receive Message %s',param_dict['Content'])
-        if param_dict['Content'] == '123':
+        if param_dict['Content'] == '你是个傻瓜':
             param_dict['Content'] = '2333'
         else:
             param_dict['Content'] = 'hello,world'
         return param_dict
 
-server = WeChatServer('demo','H:\FlaskWeb\MyFlask\MyFlask\config.ini',api)
+server = WeChatServer('demo','config.ini',api)
 
 server.register_callback('text', TestClass().reply_func)
