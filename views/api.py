400ae04ea752a30f10905155073b035b4ccc0621
SivilTaram <qianlxc@outlook.com>
2016-11-01 20:23:51 +0800

    Change the indient

--- views/api.py
+++ views/api.py
@@ -5,11 +5,11 @@ api = Blueprint('api',__name__)
 
 class TestClass(object):
     def reply_func(self,param_dict):
-	print 'Receive Message %s' % (param_dict['Content'])
-	if param_dict['Content'] == '123':
-		param_dict['Content'] = '2333'
-	else:
-        	param_dict['Content'] = 'hello,world'
+        print ('Receive Message %s',param_dict['Content'])
+        if param_dict['Content'] == '123':
+            param_dict['Content'] = '2333'
+        else:
+            param_dict['Content'] = 'hello,world'
         return param_dict
 
 server = WeChatServer('demo','H:\FlaskWeb\MyFlask\MyFlask\config.ini',api)
